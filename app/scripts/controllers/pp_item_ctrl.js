717c02164404d2188683ec9402acfb8d91ec67b8
Simon Yu <simonykq@gmail.com>
2015-01-14 08:18:02 +0000

    pp page still going

--- app/scripts/controllers/pp_item_ctrl.js
+++ app/scripts/controllers/pp_item_ctrl.js
@@ -2,88 +2,66 @@
  * Created by simonykq on 13/01/2015.
  */
 var PP_ITEM_ROUTE = (function(){
+
+    var user,
+        tweet;
+
+    function loadUser(user){
+
+        var uri = '/api/user/key/' + user;
+
+        $.ajax({
+            url: uri,
+            dataType: 'json',
+            success: function(data){
+                //TODO: change this back later to call with data;
+                updateUser();
+            },
+            error: function(xhr, type){
+                alert('Failed to load user' + user);
+            }
+        });
+    }
+
+    function loadTweet(tweet){
+
+    }
+
+
     return {
         template_url: '/views/pp_item.html',
         context: ".container",
         before_enter: function(user,pp){
 
-            $('title').text('冒泡');
-            $('#page_name').text('冒泡');
+            $('title').text(user + '的冒泡');
+            $('#page_name').text(user + '的冒泡');
 
             //add those extra items in nav menu
             $("#navigator").append( '<li class="nav-divider"></li>' +
                 '<li><a href="/pp/hot' + '">热门</a></li>'
             );
 
-            ////add actions in pp page
-            $('<div id="pp_actions" class="btn-group btn-group-justified" role="group" aria-label="...">' +
-                '<div class="btn-group" role="group">' +
-                    '<a class="btn btn-default glyphicon glyphicon-heart" data-toggle="modal" data-target="#pp_input"> 喜欢 </a>' +
-                '</div>' +
-            '</div>'
-            ).insertAfter($('#bs-example-navbar-collapse-1'));
-
-
         },
         on_enter: function(user,pp){
-            //
-            ////decide if this is hot page
-            //if(hot === 'hot'){
-            //    sort = 'hot';
-            //}else{
-            //    sort = 'time';
-            //}
-            //
-            //refresh();
-            //
-            //$('#load_more').on('click', function(e){
-            //    e.preventDefault();
-            //    loadMore('/api/tweet/public_tweets');
-            //});
-            //
-            //$('#refresh').on('click', function(e){
-            //    e.preventDefault();
-            //    refresh();
-            //});
-            //
-            //$('#pp_input').on('click', '#pp_submit', function(e){
-            //    e.preventDefault();
-            //
-            //    var content = $('#pp_content'),
-            //        btn     = $(this);
-            //
-            //    if(content.val() !== ''){
-            //
-            //        btn.attr('disabled', 'disabled');
-            //
-            //        $.post('/api/tweet', {content: content.val()}, function(data){
-            //            //fail
-            //            if(data.msg){
-            //                for(var key in data.msg){
-            //                    alert(data.msg[key]);
-            //                }
-            //            }
-            //            //successful
-            //            if(data.data){
-            //                data.data['owner'] = {}; //current user
-            //                var commentEle = createTweetDOM(data.data);
-            //                list.prepend(commentEle);
-            //
-            //                content.val('');
-            //                $('#pp_input').modal('hide');
-            //            }
-            //            btn.removeAttr('disabled');
-            //        });
-            //    }
-            //
-            //    return false
-            //});
+            //set up accordion
+            $("#user-heading .panel-title").click(function(e){
+                e.preventDefault();
+                var ele = $('#user-details');
+                if(ele.hasClass('in')){
+                    ele.collapse('hide');
+                }else{
+                    ele.collapse('show');
+                }
+                return false;
+            });
+
+            loadUser(user);
+            loadTweet(pp);
         },
         on_exit: function(){
             $('title').text('');
             $('#page_name').text('');
             $('#navigator > li').slice(-1).remove();
-            $('#pp_actions').remove();
         }
     }
 })();
