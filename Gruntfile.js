50c26938b2bc093984cfc103feb8e004213a28ed
Shuanglei Tao <tsl0922@gmail.com>
2015-01-15 11:02:33 +0800

    run on coding

--- Gruntfile.js
+++ Gruntfile.js
@@ -98,7 +98,7 @@ module.exports = function (grunt) {
         },
         connect: {
             options: {
-                port: 9000,
+                port: process.env.PORT || 9000,
                 // change this to '0.0.0.0' to access the server from outside
                 hostname: '0.0.0.0'
             },
