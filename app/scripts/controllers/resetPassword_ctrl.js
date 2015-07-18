380a37ccee7386ff8ffea56f7c950b962f341aea
wenki <wenki@Qi-MacBook-Pro.local>
2015-07-19 01:59:39 +0800

    添加重置密码页面

---
+++ app/scripts/controllers/resetPassword_ctrl.js
@@ -0,0 +1,79 @@
+/**
+ * Created by wenki on 19/07/2015.
+ */
+var RESETPASSWORD_ROUTE = (function(){
+
+    return {
+        template_url: '/views/resetPassword.html',
+        context: '.container',
+        before_enter: function(){
+            if (router.current_user) {
+                location.href = '/';
+            } 
+        },
+        on_enter: function(){
+            var refreshCaptcha = function(){
+                $('img.captcha').attr('src', API_DOMAIN + '/api/getCaptcha?code=' + Math.random());
+            }
+
+            var changeStyle = function (){
+                var controls = new Array('email','captcha');
+
+                var flag = true;
+                for (var i = controls.length - 1; i >= 0; i--) {
+                    var value = $.trim($('#' + controls[i]).val());
+                    var elem = $('.label-' + controls[i]);
+                    if (value == ''){
+                        elem.css('color','#999999');
+                        flag = false;
+                    }else{
+                        elem.css('color','#222222');
+                    }
+                };
+
+                var elem_btn = $('.btn-activate');
+                if (flag){
+                    elem_btn.removeAttr('disabled');
+                    elem_btn.css('color','#ffffff');
+                }else{
+                    elem_btn.attr('disabled','disabled');
+                    elem_btn.css('color','rgba(255,255,255,0.5)');
+                }
+            }
+
+            $('form.activate').submit(function(e) {
+                e.preventDefault();
+
+                var email = $.trim($("#email").val()),
+                    j_captcha = $.trim($("#captcha").val());
+
+                $.ajax({
+                    url: API_DOMAIN + '/api/reset_password?email='+email+'&j_captcha='+j_captcha,
+                    dataType: 'json',
+                    xhrFields: {
+                        withCredentials: true
+                    },
+                    success: function(data) {
+                        if (data.code == 0) {
+                            router.run.call(router, '/login');
+                            alert(' 已经发送邮件。');
+                        }
+                        if (data.msg) {
+                            refreshCaptcha();
+                            for(var key in data.msg) {
+                                alert(data.msg[key]);
+                            }
+                        }
+                    },
+                    error: function() {
+                        alert('Failed to activate');
+                    }
+                });
+            });
+
+            $('#email').on('input',changeStyle);
+            $('#captcha').on('input',changeStyle);
+            $('img.captcha').on('click',refreshCaptcha);
+        }
+    }
+})();
