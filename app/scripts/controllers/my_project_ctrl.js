4f753b6396c955e252aba64601e69df006e78e01
王振威 <wangzhenwei@coding.net>
2015-11-16 17:46:40 +0800

    * 在没有常用项目的时候不显示常用项目，没有一般项目的时候不显示一般项目

--- app/scripts/controllers/my_project_ctrl.js
+++ app/scripts/controllers/my_project_ctrl.js
@@ -10,19 +10,19 @@ var MY_PROJECT_ROUTE = (function(){
     // 修复图片的相对路径
     function fixRelativeURL(list, key){
         if($.isPlainObject(list)) list = [list];
-        
+
         for(var i = 0; i < list.length;i++){
             if(list[i][key].substr(0,1) === '/'){
                 list[i][key] = API_DOMAIN + list[i][key];
             }
         }
-        
+
         return list;
     }
 
     function assembleDOM(data){
         if(!data || !data.list || !data.list.length) return;
-        data.list = fixRelativeURL(data.list, "icon");  
+        data.list = fixRelativeURL(data.list, "icon");
         var newList = [];
         for(var i =0; i < data.list.length;i++){
             data.list[i].projectHomeURL = coding.projectHomePath(null, null, data.list[i]);
@@ -40,22 +40,25 @@ var MY_PROJECT_ROUTE = (function(){
             if(!exist) newList.push(data.list[i]);
         }
         data.list = newList;
-        
+        if(!data || !data.list || !data.list.length) {
+            $("#projects_list_container").hide();
+            return;
+        }
         $container = $('#projects_list');
         var template = $('#tlist').html();
         Mustache.parse(template);   // optional, speeds up future uses
         var rendered = Mustache.render(template, data);
-        
+
         $container.append(rendered);
 
     }
      function assemblePinDOM(data){
         if(!data || !data.list || !data.list.length) {
-            $("#pinProjectContainer").hide();
+            $("#project_pin_container").hide();
             return;
         }
-        
-        data.list = fixRelativeURL(data.list, "icon");  
+
+        data.list = fixRelativeURL(data.list, "icon");
         for(var i =0; i < data.list.length;i++){
             data.list[i].projectHomeURL = coding.projectHomePath(null, null, data.list[i]);
             //加载完项目信息后，将未读动态数超过 100 的设为 99+
@@ -63,14 +66,14 @@ var MY_PROJECT_ROUTE = (function(){
                 data.list[i].un_read_activities_count = '99+';
             }
         }
-        
+
         $container = $('#projects_pin');
         var template = $('#tlist').html();
         Mustache.parse(template);   // optional, speeds up future uses
         var rendered = Mustache.render(template, data);
-        
+
         $container.append(rendered);
-    }   
+    }
     function loadPinProjects(){
         coding.get('/api/user/projects/pin?pageSize=9999',function(data){
             pinList = data.data.list;
@@ -78,7 +81,7 @@ var MY_PROJECT_ROUTE = (function(){
             loadMore();
         });
     }
-    
+
     function loadMore(){
         var path = '/api/projects';
         pageCount++;
@@ -128,10 +131,10 @@ var MY_PROJECT_ROUTE = (function(){
                 alert('You are not logged in');
                 return;
             }
-            
+
             loadPinProjects();
-            
-            
+
+
             var element = $("#load_more");
             element.on('click', function(e){
                 e.preventDefault();
